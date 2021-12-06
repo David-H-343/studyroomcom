1bbd2ac937fc94dc37216f2963c53ceffd69f229
David Harmon <harmon.david343@gmail.com>
2021-12-05 22:35:22 -0700

    Removed unused code

--- src/templates/MainTemplate.tsx
+++ src/templates/MainTemplate.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { Layout, Typography, Menu, Row, Col, Affix } from 'antd';
 import PageNavigation from './PageNavigation';
 import {
